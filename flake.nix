53ba0008cdf8245341985f9fdf434179e86199c5
Tom Bereknyei <tomberek@gmail.com>
2022-04-02 15:17:00 -0400

    callLockless flake from flake-compat

--- flake.nix
+++ flake.nix
@@ -7,25 +7,39 @@ rec {
   inputs.brother.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/21.11";
 
-  outputs = { self, nixpkgs,... }@args: {
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ args: {
     children = {inherit (args) brother sister;};
     value = "parent";
     siblingValue = "parent";
     values = map (x: x.value) ([self] ++ (builtins.attrValues self.children));
 
+    hash = builtins.hashFile "sha256" ./brother;
+    t = (builtins.getFlake "path:./brother").value;
+
     getWithParent = args.brother.value;
 
     getWithChild = let
-       c = builtins.getFlake (builtins.unsafeDiscardStringContext "path:./brother?narHash=${args.brother.sourceInfo.narHash}");
-    in c.value;
+      c = builtins.getFlake (builtins.unsafeDiscardStringContext "path:./brother?narHash=${args.brother.sourceInfo.narHash}");
+    in
+      c.value;
 
-    dynamicGetWithParent = (let r = (import ./brother/flake.nix).outputs (args.brother.inputs // { self=r;}); in r ).value;
+    callLocklessFlake = path: let
+      r = {outPath = path;} //
+      (import (path + "/flake.nix")).outputs (args // {self = r;});
+    in
+      r;
+
+    dynamicGetWithParent = self.callLocklessFlake ./brother;
 
     dynamicGetWithChild = let
       brother = import ./brother/default.nix;
       brotherFunc = import ./brother/flake.nix;
-      r = brotherFunc.outputs (brother.inputs // { self=r;});
-    in r.value;
-
+      r = brotherFunc.outputs (brother.inputs // {self = r;});
+    in
+      r.value;
   };
 }
